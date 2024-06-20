79212a89deb881321331a2b2be2e70e2642b360e
rutwik2514 <rutwik2514@gmail.com>
2024-06-20 20:01:25 +0000

    Initial commit

---
+++ utils/permissions.js
@@ -0,0 +1,6 @@
+exports.PERMISSIONS = ["createNested", "updateNested", "deleteNested", "readNested", "createdockerschema", "updatedockerschema", "deletedockerschema", "readdockerschema"]; 
+exports.ROLES = ["unrestricted", "Restricted", "unrestricted2"] 
+exports.UNRESTRICTED = ["unrestrictedPermission1"]; 
+exports.RESTRICTED = ["RestrictedPermission1"]; 
+exports.UNRESTRICTED2 = ["unrestrictedPermission1"]; 
+exports.RESTRICTED_ROLES = ["Restricted", "unrestricted"]; 
